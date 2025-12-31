9bdfa413bdee74f4950884fe3b0c5c26a738c0ca
AbdelrhmanKhaled76 <bodi.khaled24@gmail.com>
2025-12-31 09:30:42 +0200

    fix : but in lab5

--- Client Side Technologies/CST-Day5/lab5/task1.js
+++ Client Side Technologies/CST-Day5/lab5/task1.js
@@ -1,29 +1,29 @@
 const arr = [];
+
 let size = Number(prompt("Enter a number to specify a size for the Array"));
 
-while(isNaN(size) || !size){
-    size = Number(prompt("please enter a valid size !"));
+while (isNaN(size) || size <= 0) {
+    size = Number(prompt("Please enter a valid array size (positive number)!"));
 }
 
-for(let i=0; i < size; i++ ){
-    let value = Number(prompt("enter the " + (i+1) + " number for the array"));
-    while(isNaN(value) || !value){
-        prompt("please enter a valid size !");
+for (let i = 0; i < size; i++) {
+    let value = Number(prompt("Enter number " + (i + 1)));
+
+    while (isNaN(value)) {
+        value = Number(prompt("Please enter a valid number!"));
     }
+
     arr.push(value);
 }
 
-console.log("this is the original array : ", arr);
+console.log("This is the original array:", arr);
 
-arr.sort(function(a, b){
-    return a-b;
-})
+arr.sort(function(a, b) {
+    return a - b
+});
+console.log("Ascending sorted version:", arr);
 
-console.log("the ascending sorted version : ", arr);
-
-
-arr.sort(function(a, b){
-    return b-a;
-})
-
-console.log("the ascending sorted version : ", arr);
+arr.sort(function(a, b) {
+    return b - a
+});
+console.log("Descending sorted version:", arr);
--- Client Side Technologies/CST-Day5/lab5/task3.js
+++ Client Side Technologies/CST-Day5/lab5/task3.js
@@ -1,45 +1,41 @@
 //first part
 
-let childWin;
-let intervalId;
-let y = 0;
-let step = 10;
-let direction = 1;
+// let childWin;
+// let intervalId;
+// let y = 0;
+// let step = 10;
+// let direction = 1;
 
-function openWindow() {
-  y = 0;
-  direction = 1;
+// function openWindow() {
+//   y = 0;
+//   direction = 1;
 
-  childWin = window.open(
-    "child.html",
-    "",
-    "width=300,height=200,alwaysRaised=yes"
-  );
+//   childWin = window.open(
+//     "child.html",
+//     "",
+//     "width=300,height=200,alwaysRaised=yes"
+//   );
 
-  intervalId = setInterval(() => {
-    if (!childWin || childWin.closed) {
-      stopMoving();
-      return;
-    }
+//   intervalId = setInterval(() => {
 
-    const maxY = screen.availHeight - 200;
-    y += step * direction;
+//     const maxY = screen.availHeight - 200;
+//     y += step * direction;
 
-    if (y <= 0 || y >= maxY) {
-      direction *= -1;
-    }
+//     if (y <= 0 || y >= maxY) {
+//       direction *= -1;
+//     }
 
-    y = Math.max(0, Math.min(y, maxY));
+//     y = Math.max(0, Math.min(y, maxY));
 
-    childWin.moveTo(300, y);
-    childWin.focus();
-  }, 50);
-}
+//     childWin.moveTo(300, y);
+//     childWin.focus();
+//   }, 50);
+// }
 
-function closeWindow() {
-  clearInterval(intervalId);
-  intervalId = null;
-}
+// function closeWindow() {
+//   clearInterval(intervalId);
+//   intervalId = null;
+// }
 
 
 //second part
@@ -51,9 +47,6 @@ function closeWindow() {
 //   adWin = window.open("child.html", "", "width=400,height=300");
 
 //   intervalId = setInterval(() => {
-//       clearInterval(intervalId);
-
-
 //     adWin.scrollBy(0, 2);
 //     adWin.focus();
 //   }, 50);
@@ -65,20 +58,20 @@ function closeWindow() {
 
 //second
 
-// let adWin;
-// let timeoutId;
+let adWin;
+let timeoutId;
 
-// function openWindow() {
-//   adWin = window.open("child.html", "", "width=400,height=300");
-//   scrollAd();
-// }
+function openWindow() {
+  adWin = window.open("child.html", "", "width=400,height=300");
+  scrollAd();
+}
 
-// function scrollAd() {
-//   adWin.scrollBy(0, 2);
-//   adWin.focus()
-//   timeoutId = setTimeout(scrollAd, 50);
-// }
+function scrollAd() {
+  adWin.scrollBy(0, 2);
+  adWin.focus()
+  timeoutId = setTimeout(scrollAd, 50);
+}
 
-// function closeWindow() {
-//   clearTimeout(timeoutId);
-// }
+function closeWindow() {
+  clearTimeout(timeoutId);
+}
