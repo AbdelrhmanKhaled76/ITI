65df5a97676135002d17b19312bcdef50ece6fc1
AbdelrhmanKhaled76 <bodi.khaled24@gmail.com>
2026-01-08 09:51:44 +0200

    feat : task2 is done

--- Client Side Technologies/CST-Day8/CST-Day8/lab/task2/task2.js
+++ Client Side Technologies/CST-Day8/CST-Day8/lab/task2/task2.js
@@ -1,7 +1,16 @@
 function add(a,b){
     if(arguments.length !== 2)
         throw new Error("invalind number of arguments !!");
-    else if(!Number(a) || !Number(b))
-        throw new TypeError("inputs must be numbers !!");
     return a + b;
 }
+
+// function add(){
+//     let additionResult = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         if(typeof arguments[i] !== "number")
+//             throw new TypeError("only number type is valid !!");
+//         additionResult += Number(arguments[i]);
+//     }
+//     return additionResult;
+// }
+console.log(add(1,3));
